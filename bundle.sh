dd1036011d2c936bdecbdab04be573194e6d01f0
nyaa8 <contrib@n8.pm>
2021-01-11 01:32:25 +0100

    Use csh instead of plain sh in bundle.sh
    This should prevent incompatibilities between different default sh
    interpreters used across different operating systems

--- bundle.sh
+++ bundle.sh
@@ -1,9 +1,11 @@
-#!/bin/bash
-if [ ! -d ~/.config/nvim/pack/packager/opt/vim-packager ]; then
+#!/bin/csh
+set config_files=(plugins.vim settings.vim theme.vim lsp.vim)
+if ( ! -d ~/.config/nvim/pack/packager/opt/vim-packager ) then
 	echo "Preparing vim-packager"
 	git clone https://github.com/kristijanhusak/vim-packager ~/.config/nvim/pack/packager/opt/vim-packager
-fi
+endif
 
-cat ./partials/*.vim | sed -e '/^"/ d' -e '/^\s*$/ d' > init.vim
+cd ~/.config/nvim/partials
+cat $config_files | sed -e '/^"/ d' -e '/^\s*$/ d' > ../init.vim
 nvim +PackagerInstall
 
